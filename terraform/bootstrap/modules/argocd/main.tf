59752efccc01b2a3a5cfbe97a2ea9b542948741a
maxlim0 <none@maxlim0.com>
2026-02-05 19:50:23 +0100

    cert manager cleanup

--- terraform/bootstrap/modules/argocd/main.tf
+++ terraform/bootstrap/modules/argocd/main.tf
@@ -1,16 +1,3 @@
-# resource "kubernetes_namespace_v1" "this" {
-#   count = var.create_namespace ? 1 : 0
-
-#   metadata {
-#     name = var.namespace
-
-#     labels = {
-#       "app.kubernetes.io/part-of" = "argocd"
-#       "platform.layer"            = "bootstrap"
-#     }
-#   }
-# }
-
 resource "helm_release" "argocd" {
   name       = "argocd"
   namespace  = var.namespace
@@ -26,12 +13,12 @@ resource "helm_release" "argocd" {
   cleanup_on_fail  = true
 
   values = [
-    file("${path.module/argocd-values.yaml}")
+    file("${path.module}/argocd-values.yaml")
   ]
 
-  depends_on = [
-    kubernetes_namespace_v1.this
-  ]
+  # depends_on = [
+  #   kubernetes_namespace_v1.this
+  # ]
 }
 
 resource "time_sleep" "wait_for_argocd_secret" {
@@ -40,7 +27,7 @@ resource "time_sleep" "wait_for_argocd_secret" {
   create_duration = "30s"
 }
 
-data "kubernetes_secret" "argocd_initial_admin" {
+data "kubernetes_secret_v1" "argocd_initial_admin" {
   metadata {
     name      = "argocd-initial-admin-secret"
     namespace = "argocd"
--- terraform/bootstrap/modules/cert-manager/main.tf
+++ terraform/bootstrap/modules/cert-manager/main.tf
@@ -7,16 +7,3 @@ resource "kubernetes_namespace_v1" "cert_manager" {
     name = "cert-manager"
   }
 }
-
-# resource "kubernetes_service_account_v1" "cert_manager" {
-#   metadata {
-#     name      = "cert-manager"
-#     namespace = "cert-manager"
-
-#     annotations = {
-#       "eks.amazonaws.com/role-arn" = var.cert_manager_role_arn
-#     }
-#   }
-
-#   depends_on = [kubernetes_namespace_v1.cert_manager]
-# }
